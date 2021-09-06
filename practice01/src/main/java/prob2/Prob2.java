a8d66ba263de19413234fa26c4a859ffb930ee59
soottoppi <duwjs1103@naver.com>
2021-09-06 19:37:10 +0900

    First Commit!

---
+++ practice01/src/main/java/prob2/Prob2.java
@@ -0,0 +1,13 @@
+package prob2;
+
+public class Prob2 {
+	public static void main(String[] args) {
+		
+		for(int i=1; i<11; i++) {
+			for(int j=i; j<i+10; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ practice01/src/main/java/prob3/Prob3.java
@@ -0,0 +1,33 @@
+package prob3;
+
+import java.util.Scanner;
+
+public class Prob3 {
+	
+	public static void main(String[] args) {
+		int num;
+		int resultValue = 0;
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력하세요 : ");
+		num = scanner.nextInt();
+		if(num > 0) {
+			if(num % 2 == 0) {
+				for(int i=0; i<=num ; i+=2) {
+					resultValue += i; 
+				}
+				System.out.println("결과 값 : " + resultValue);
+
+			} else {
+				for(int i=1; i<=num; i+=2) {
+					resultValue += i;
+				}
+				System.out.println("결과 값 : " + resultValue);
+			}
+		}
+		
+		
+		
+		scanner.close();
+	}
+}
