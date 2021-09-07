5fee748820c749e71fd5a0991abff0c444ad863f
soottoppi <duwjs1103@naver.com>
2021-09-07 11:10:24 +0900

    prob5 continuing..

--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -4,26 +4,28 @@ public class Prob5 {
 
 	public static void main(String[] args) {
 		int numLength;
+		int rem;
+		
+		
 		for(int i=1; i<100; i++) {
+			boolean isJJak = false;
 			String resultText ="";
-			int div = 10;
+			int div = 1;
 			
 			numLength = (int)(Math.log10(i)+1);
-			for(int j=0; j<numLength; j++) {
-			
+			for(int j=0; j < numLength; j++) {
+				rem = i % 10;
+				
 				if(i % div == 3 || i % div == 6 || i % div == 9) {
 					resultText += "짝";
+					isJJak = true;
 				}
-				div *= 10;
-			}
-			System.out.println(i + " " + resultText);
-			
+				rem /= 10;
 				
-				/* 
-				 	일의 자리가 3,6,9 인 경우
-					십의자리가 3,6,9 인경우
-				    ... 결국 각 자리에 하나라도(||) 3,6,9 존재 시 짝 출력
-				*/
+			}
+			if(isJJak) {
+				System.out.println(i + " " + resultText);	
+			}
 			
 		}
 		
