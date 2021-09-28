6d620f61d9c1677ac37ad128ab8e6a7aed4f3762
soottoppi <duwjs1103@naver.com>
2021-09-28 17:13:52 +0900

    test commit

--- network/src/main/java/echo/EchoServer.java
+++ network/src/main/java/echo/EchoServer.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 
 public class EchoServer {
 	private static final String SERVER_IP = "127.0.0.1";
-	private static final int SERVER_PORT = 6000;
+	private static final int SERVER_PORT = 8000;
 
 	public static void main(String[] args) {
 		ServerSocket serverSocket = null;
